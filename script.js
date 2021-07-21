484124b61844dfcff8c71dc16fb95a9c13982867
suraj32 <suraj202003@gmail.com>
2021-07-22 01:17:44 +0530

    created loop for random hole creation,made it to display score,high score, added a audio clip when we lose

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ var raf;
 
 ctx.fillRect(0, 0, 700, 125);
 ctx.fillRect(0, 375, 700, 125);
+function replay() { location.reload(); }
 
 var box = {
     x: 100,
@@ -63,24 +64,33 @@ function move() {
     box.draw();
     hole.draw();
     console.log("movehole");
-    if (status == "floor") {
-        if (hole.x - hole.vx < -125) {
-            window.cancelAnimationFrame(raf);
-        }
-        else {
-            hole.x -= hole.vx;
-            raf = window.requestAnimationFrame(move);
-        }
+    if (t==0){
+        createHole(Math.floor(Math.random()*10));
+    }
+    else if((sec-t)>4){
+        createHole(Math.floor(Math.random()*10));
+    }
+}
+let rafCount=0;
+function createHole(x){
+    if (x<5 && hole.x==700) {
+        hole.y = 375;
+    }
+    else if(x>4 && hole.x==700){
+        hole.y = 0;
+    }
+    if (hole.x - hole.vx < -125) {
+        window.cancelAnimationFrame(raf);
+        hole.x=700;
+        raf = window.requestAnimationFrame(move);
     }
     else {
-        if (hole.x - hole.vx < -125) {
-            window.cancelAnimationFrame(raf);
-        }
-        else {
-            hole.x -= hole.vx;
-            raf = window.requestAnimationFrame(move);
-        }
+        hole.x -= hole.vx;
+        raf = window.requestAnimationFrame(move);
     }
+    if(((box.x+74)==hole.x && (box.y+75)==hole.y) || ((box.x+74)==hole.x && (box.y-125)==hole.y)){
+            end();
+        }
 }
 
 function swift() {
@@ -109,25 +119,14 @@ function swift() {
     }
 }
 
-/*canvas.addEventListener('mousemove', function(e) {
-  if (!running) {
-    ctx.clearRect(0,125,700,250);
-    box.x = e.clientX;
-    box.y = e.clientY;
-    box.draw();
-  }
-});*/
-
-start = document.getElementById('start');
+let sec=0,t=0;
+function timer(){
+    sec+=0.1;
+}
+var start = document.getElementById('start');
 start.addEventListener('click', function () {
-    if (status == "floor") {
-        hole.x = 700;
-        hole.y = 375;
-    }
-    else {
-        hole.x = 700;
-        hole.y = 0;
-    }
+    interval = window.setInterval(timer,100);
+    start.style.display = "none";
     raf = window.requestAnimationFrame(move);
 });
 
@@ -136,10 +135,32 @@ canvas.addEventListener('click', function () {
     console.log(status);
 });
 
-/*canvas.addEventListener('mouseout', function(e) {
-  window.cancelAnimationFrame(raf);
-  running = false;
-});*/
-
 box.draw();
 hole.draw();
+
+var modal = document.getElementById("modal");
+modal.style.display = "none";
+function end(){
+    console.log("...intersection...");
+    window.cancelAnimationFrame(flip);
+    window.cancelAnimationFrame(raf);
+    window.clearInterval(interval);
+    console.log(sec);
+    new Audio("Disgruntled Gramophone.mp3").play();
+    modal.style.display = "block";
+    displayScore();
+}
+
+function displayScore() {
+    distance = 60*sec; //in px //As animation is in 60 frames per sec
+    score = Math.floor(distance<2000 ? distance : distance<4000 ? distance*1.2 : distance<6000 ? distance*1.4 : dsitance*1.6);
+    hs = localStorage.getItem("HiSc");    
+    if (hs == Infinity) {
+        hs = score;
+    } else if (score > hs) {
+        hs = score;
+    }
+    localStorage.setItem("HiSc", hs);
+    document.getElementById("score").innerHTML = "Your Score: " + score;
+    document.getElementById("highscore").innerHTML = "⚡High Score:" + hs;
+}
