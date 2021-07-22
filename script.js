f4ab8390197220917c430036b80d20f74cc32548
suraj32 <suraj202003@gmail.com>
2021-07-22 20:35:00 +0530

    wrote description for game, created branch and developed obstacle arriving

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var glide;
+var glideHole;
+var glideBall;
+var flip;
 var speed=1;
 
 ctx.fillRect(0, 0, 700, 125);
@@ -25,12 +27,27 @@ var hole = {
         console.log(speed);
         ctx.fillStyle = 'rgb(255,255,255)';
         ctx.fillRect(this.x, this.y, 125, 125);
-        //console.log("hole");
+        console.log("hole");
+    }
+};
+var obstacle = {
+    x: 725,
+    y: 350,
+    vx: 2,
+    vy: 29,
+    draw: function () {
+        objSpeed = sec<20 ? 1:sec<40 ? 2:sec<80 ? 3:sec<120 ? 4:6
+        //console.log(speed);
+        ctx.fillStyle = 'rgb(100,0,0)';
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,25,0,2*Math.PI);
+        ctx.fill();
+        console.log("obstacle");
     }
 };
 status = "floor";
 
-let sec=0,t=0;
+let sec=0;
 function timer(){
     sec+=0.1;
 }
@@ -38,10 +55,14 @@ var start = document.getElementById('start');
 start.addEventListener('click', function () {
     interval = window.setInterval(timer,100);
     start.style.display = "none";
-    glide = window.requestAnimationFrame(move);
+    glideHole = window.requestAnimationFrame(moveHole);
+    //glideBall=window.requestAnimationFrame(moveObstacle);
+    setTimeout(function() {
+        glideBall=window.requestAnimationFrame(moveObstacle);
+    }, 3.35*1000);
+  
 });
 
-var flip;
 canvas.addEventListener('click', function () {
     flip = window.requestAnimationFrame(swift);
     console.log(status);
@@ -53,19 +74,16 @@ document.body.addEventListener('keydown', function (e) {
     }
 });
 
-function move() {
+function moveHole() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, 700, 125);
     ctx.fillRect(0, 375, 700, 125);
+    ctx.clearRect(0,125,700,250);
     box.draw();
     hole.draw();
+    obstacle.draw();
     console.log("movehole");
-    if (t==0){
-        createHole(Math.floor(Math.random()*10));
-    }
-    else if((sec-t)>4){
-        createHole(Math.floor(Math.random()*10));
-    }
+    createHole(Math.floor(Math.random()*10));
 }
 
 function createHole(x){
@@ -76,15 +94,67 @@ function createHole(x){
         hole.y = 0;
     }
     if (hole.x - hole.vx < -125) {
-        window.cancelAnimationFrame(glide);
+        window.cancelAnimationFrame(glideHole);
         hole.x=700;
-        glide = window.requestAnimationFrame(move);
+        glideHole = window.requestAnimationFrame(moveHole);
     }
     else {
         hole.x -= hole.vx*speed;
-        glide = window.requestAnimationFrame(move);
+        glideHole = window.requestAnimationFrame(moveHole);
     }
     if(((box.x+74)>hole.x && (box.x+75)<hole.x+125 && (((box.y-125)==hole.y) || (box.y+75)==hole.y)) ){
+            end();
+        }
+}
+let update = 'up';
+function moveObstacle(){
+    ctx.clearRect(0, 125, 700, 250);
+    box.draw();
+    hole.draw();
+    obstacle.draw();
+    console.log("moveobstacle");
+    createObstacle(Math.floor(Math.random()*10));
+}
+function createObstacle(m){
+    if (m<5 && obstacle.x==725) {
+        obstacle.y = 350;
+        update = 'up';
+    }
+    else if(m>4 && obstacle.x==725){
+        obstacle.y = 150;
+        update = 'down';
+    }
+    if (obstacle.x - obstacle.vx < -25) {
+        window.cancelAnimationFrame(glideBall);
+        obstacle.x=725;
+        glideBall = window.requestAnimationFrame(moveObstacle);
+    }
+    else {
+        obstacle.x -= obstacle.vx*objSpeed;
+        if(update == 'up'){
+            obstacle.y -= obstacle.vy;
+            console.log(update);
+            if(obstacle.y == 150){
+                update = 'down';
+                console.log(update);
+            }
+        }
+        else{
+            obstacle.y += obstacle.vy;
+            console.log(update);
+            if(obstacle.y == 350){
+                update = 'up';
+                console.log(update);
+            }
+        }
+        glideBall = window.requestAnimationFrame(moveObstacle);
+    }
+    d = Math.pow(Math.pow((box.x+37.5-obstacle.x),2) + Math.pow((box.y+37.5-obstacle.y),2),0.5);
+    if( ((box.x+75==obstacle.x-25)&&(d<72.887)) || ((box.y+75==obstacle.y-25)&&(d<72.887)) 
+    || Math.pow(Math.pow((box.x-obstacle.x),2) + Math.pow((box.y-obstacle.y),2),0.5)==25 
+    || Math.pow(Math.pow((box.x+75-obstacle.x),2) + Math.pow((box.y-obstacle.y),2),0.5)==25
+    || Math.pow(Math.pow((box.x-obstacle.x),2) + Math.pow((box.y+75-obstacle.y),2),0.5)==25
+    || Math.pow(Math.pow((box.x+75-obstacle.x),2) + Math.pow((box.y+75-obstacle.y),2),0.5)==25 ){
             end();
         }
 }
@@ -92,6 +162,7 @@ function createHole(x){
 function swift() {
     ctx.clearRect(0, 125, 700, 250);
     box.draw();
+    obstacle.draw();
     if(box.y==215){ new Audio("when-604.mp3").play();}
     console.log(status);
     if (status == "floor") {
@@ -118,13 +189,15 @@ function swift() {
 
 box.draw();
 hole.draw();
+obstacle.draw();
 
 var modal = document.getElementById("modal");
 modal.style.display = "none";
 function end(){
     console.log("...intersection...");
     window.cancelAnimationFrame(flip);
-    window.cancelAnimationFrame(glide);
+    window.cancelAnimationFrame(glideHole);
+    window.cancelAnimationFrame(glideBall);
     window.clearInterval(interval);
     console.log(sec);
     new Audio("Disgruntled Gramophone.mp3").play();
